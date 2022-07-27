a6e9051a626a81a0cdc98c15383c1d86ca89dcc1
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-07-27 19:08:45 +0530

    Add

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,22 +1,52 @@
+import { Component, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [personState, setpersoneState] = useState({
+    person: [
+      { name: "Max", age: 20 },
+      { name: "Mini", age: 10 },
+    ],
+  });
+
+  const [showPerson, setShowPerson] = useState(false);
+
+  const handleChange = (userName, age) => {
+    setpersoneState({
+      person: { name: userName, age: age },
+      // person: { name: "Max", age: 20 },
+    });
+  };
+
+  const toggleHandler = () => {
+    // setShowPerson(true);
+    const doesShow = showPerson;
+    setShowPerson(!doesShow);
+  };
+
+  let person;
+
+  if (showPerson) {
+    person = (
+      <div>
+        <h1>
+          {personState.person.map((per, i) => {
+            return ` ${i + 1} >  Hello my name is ${per.name} and my age is ${
+              per.age
+            }`;
+          })}
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <>
       <div>
-        <h1>Hello this is number one bull shit in react app</h1>
-      </div>
-      <div>
-        <h1>Hello this is number one bull shit in react app</h1>
-      </div>
-      <div>
-        <h1>Hello this is number one bull shit in react app</h1>
-      </div>
-      <div>
-        <h1>Hello this is number one bull shit in react app</h1>
-      </div>
-      <div>
-        <h1>Hello this is number one bull shit in react app</h1>
+        {/* <button onClick={() => handleChange("Max!")}>Click</button> */}
+        <button onClick={toggleHandler}>Click</button>
+        <button onClick={handleChange.bind(this, "Max!!", 24)}>Click</button>
+        <div>{person}</div>
       </div>
     </>
   );
