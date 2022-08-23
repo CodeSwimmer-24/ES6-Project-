910069efc2ef339e9ef669a916e474bb64966ec8
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-23 23:00:13 +0530

    Burder.js

--- learning/src/BurgerBuilder/containers/BurgerBuilder/BurgerBuilder.js
+++ learning/src/BurgerBuilder/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
 import Aux from "../../HOC/Aux";
 
 export default class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meet: 2,
+    },
+  };
+
   render() {
     return (
       <Aux>
-        <div>Burger</div>
-        <div>Build Container</div>
+        <Burger ingredients={this.state.ingredients} />
       </Aux>
     );
   }
