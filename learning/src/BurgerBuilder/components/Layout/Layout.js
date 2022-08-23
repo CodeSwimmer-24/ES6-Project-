742075a0811f559a1e472a09e246ff402cb5d226
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-23 23:00:51 +0530

    Layout

--- learning/src/BurgerBuilder/components/Layout/Layout.js
+++ learning/src/BurgerBuilder/components/Layout/Layout.js
@@ -1,11 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import Aux from "../../HOC/Aux";
+
+const Main = styled.main`
+  margin-top: 16px;
+`;
 
 const layout = (props) => {
   return (
     <Aux>
       <div>Tools,sidebar,Backdrop</div>
-      <main>{props.children}</main>
+      <Main>{props.children}</Main>
     </Aux>
   );
 };
