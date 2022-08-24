8934017d46460daad9115c9d0d044f7e8c12b9e2
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-24 21:21:04 +0530

    Commit

--- learning/src/BurgerBuilder/containers/BurgerBuilder/BurgerBuilder.js
+++ learning/src/BurgerBuilder/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Burger from "../../components/Burger/Burger";
 import Aux from "../../HOC/Aux";
 
 export default class BurgerBuilder extends Component {
   state = {
     ingredients: {
-      salad: 1,
-      bacon: 1,
-      cheese: 2,
-      meet: 2,
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meet: 0,
     },
   };
 
@@ -16,6 +17,7 @@ export default class BurgerBuilder extends Component {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Aux>
     );
   }
