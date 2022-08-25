6a9762223ee0e84d056cd6df0183ac6d510deae4
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-08-25 17:18:41 +0530

    Added

--- learning/src/BurgerBuilder/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ learning/src/BurgerBuilder/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,12 @@ import "./BuildControl.css";
 const BuildControl = (props) => (
   <div className="BuildControl">
     <div className="Label">{props.label}</div>
-    <button className="Button">Less</button>
-    <button className="Button">More</button>
+    <button className="Button" onClick={props.remove} disabled={props.disabled}>
+      Less
+    </button>
+    <button className="Button" onClick={props.added}>
+      More
+    </button>
   </div>
 );
 
