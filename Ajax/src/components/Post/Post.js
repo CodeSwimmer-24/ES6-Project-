1f49eef51252acb82878f578add7e3472e77db9b
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-09-04 13:47:03 +0530

    HTTP ADD

--- Ajax/src/components/Post/Post.js
+++ Ajax/src/components/Post/Post.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./Post.css";
 
-function Post() {
-  return <div>Post</div>;
+function Post(props) {
+  return (
+    <div style={{ display: "flex" }}>
+      <div>
+        <article className="Post" onClick={props.clicked}>
+          <h1>{props.title}</h1>
+          <div className="Info">
+            <div className="Author">{props.author}</div>
+          </div>
+        </article>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
