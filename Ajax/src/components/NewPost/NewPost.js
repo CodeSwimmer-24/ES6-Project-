0c5fcb1664565544b2056c4019bce4381905a874
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-09-04 13:46:30 +0530

    HTTP ADD

--- Ajax/src/components/NewPost/NewPost.js
+++ Ajax/src/components/NewPost/NewPost.js
@@ -1,7 +1,51 @@
-import React from "react";
+import axios from "../../axios";
+import React, { Component } from "react";
 
-function NewPost() {
-  return <div>NewPost</div>;
+export default class NewPost extends Component {
+  state = {
+    title: "",
+    content: "",
+    author: "",
+  };
+
+  postDataHandler = () => {
+    const post = {
+      title: this.state.title,
+      content: this.state.content,
+      author: this.state.author,
+    };
+    axios.post(`/posts`, post).then((res) => {
+      console.log(res);
+    });
+  };
+
+  render() {
+    return (
+      <div className="NewPost">
+        <h1>Add a Post</h1>
+        <label>Title</label>
+        <input
+          name="title"
+          type="text"
+          value={this.state.title}
+          onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+        />
+        <label>Content</label>
+        <textarea
+          name="content"
+          rows="4"
+          value={this.state.content}
+          onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+        />
+        <label>Author</label>
+        <input
+          name="author"
+          rows="4"
+          value={this.state.author}
+          onChange={(e) => this.setState({ [e.target.name]: e.target.value })}
+        />
+        <button onClick={this.postDataHandler}>Add post</button>
+      </div>
+    );
+  }
 }
-
-export default NewPost;
