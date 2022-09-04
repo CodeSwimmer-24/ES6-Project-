1b7abde877819021418694adc9d3e3cbf7166ed8
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-09-04 13:47:19 +0530

    HTTP ADD

--- Ajax/src/container/Blog/Blog.js
+++ Ajax/src/container/Blog/Blog.js
@@ -2,24 +2,58 @@ import React, { Component } from "react";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import Post from "../../components/Post/Post";
-import axios from "axios";
+import axios from "../../axios";
 
 export default class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: "",
+    error: false,
+  };
   componentDidMount() {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      console.log(response);
-    });
+    axios
+      .get("/posts")
+      .then((response) => {
+        const posts = response.data.slice(0, 4);
+        const updatedPost = posts.map((post) => {
+          return {
+            ...post,
+            author: "Max",
+          };
+        });
+        this.setState({ posts: updatedPost });
+      })
+      .catch((error) => {
+        this.setState({ error: true });
+      });
   }
+
+  postClick = (id) => {
+    this.setState({ selectedPostId: id });
+  };
   render() {
+    let posts = (
+      <p style={{ textAlign: "center", color: "red" }}>Something went wrong!</p>
+    );
+
+    if (!this.state.error) {
+      posts = this.state.posts.map((post) => {
+        return (
+          <Post
+            title={post.title}
+            key={post.id}
+            author={post.author}
+            clicked={() => this.postClick(post.id)}
+          />
+        );
+      });
+    }
+
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
