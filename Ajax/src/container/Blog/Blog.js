fea075dc4a146089976ee143cc0917889e621389
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-09-11 22:37:55 +0530

    Hello

--- Ajax/src/container/Blog/Blog.js
+++ Ajax/src/container/Blog/Blog.js
@@ -1,75 +1,37 @@
 import React, { Component } from "react";
-import FullPost from "../../components/FullPost/FullPost";
+import { NavLink, Redirect, Route, Switch } from "react-router-dom";
 import NewPost from "../../components/NewPost/NewPost";
-import Post from "../../components/Post/Post";
-import axios from "../../axios";
+import asyncComponent from "../../HOC/asyncComponent";
+import Posts from "../Posts/Posts";
+// import NewPost from "../../components/NewPost/NewPost";
 
+const AsyncNewPost = asyncComponent(() => {
+  return import("../../components/NewPost/NewPost");
+});
 export default class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: "",
-    error: false,
-  };
-  componentDidMount() {
-    axios
-      .get("/posts")
-      .then((response) => {
-        const posts = response.data.slice(0, 4);
-        const updatedPost = posts.map((post) => {
-          return {
-            ...post,
-            author: "Max",
-          };
-        });
-        this.setState({ posts: updatedPost });
-      })
-      .catch((error) => {
-        this.setState({ error: true });
-      });
-  }
-
-  postClick = (id) => {
-    this.setState({ selectedPostId: id });
-  };
   render() {
-    let posts = (
-      <p style={{ textAlign: "center", color: "red" }}>Something went wrong!</p>
-    );
-
-    if (!this.state.error) {
-      posts = this.state.posts.map((post) => {
-        return (
-          <Post
-            title={post.title}
-            key={post.id}
-            author={post.author}
-            clicked={() => this.postClick(post.id)}
-          />
-        );
-      });
-    }
-
     return (
       <div>
         <header>
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <NavLink to="/" exact>
+                  Home
+                </NavLink>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <NavLink to="/new-post">New Post</NavLink>
               </li>
             </ul>
           </nav>
         </header>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <Switch>
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/" component={Posts} />
+          {/* <Redirect from="/" to="/post" /> */}
+          {/* <Route path="/post/:id" component={FullPost} /> */}
+        </Switch>
       </div>
     );
   }
