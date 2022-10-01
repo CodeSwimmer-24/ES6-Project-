0a83a7890063756423093b0a25159b7df13d6335
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-10-01 20:46:08 +0530

    Commit

--- learning/src/BurgerBuilder/containers/Checkout/ContactData/ContactData.js
+++ learning/src/BurgerBuilder/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Button from "../../../components/UI/Buttons/Button";
 import "./ContactData.css";
+import Spinner from "../../../components/UI/spinner/Spinner";
+import axios from "../../../../axios-orders";
 
 export default class ContactData extends Component {
   state = {
@@ -10,18 +12,57 @@ export default class ContactData extends Component {
       street: "",
       postNo: "",
     },
+    loading: false,
   };
+
+  orderHandler = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      pricing: this.props.totalPrice,
+      customer: {
+        name: "Fahad",
+        address: {
+          street: "Street no 10",
+          zip: "416152",
+          country: "India",
+        },
+        email: "fahad@gmail.com",
+      },
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => {
+        this.setState({ loading: false });
+        this.props.history.push("/");
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+        console.log(err);
+      });
+  };
+
   render() {
+    console.log(this.props);
+    let form = (
+      <form>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="email" name="email" placeholder="Email" />
+        <input type="text" name="street" placeholder="Street" />
+        <input type="text" name="postNo" placeholder="POST No" />
+        <Button btnType="success" clicked={this.orderHandler}>
+          ORDER
+        </Button>
+      </form>
+    );
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
     return (
       <div className="contact">
         <h4>Enter Your contact details</h4>
-        <form>
-          <input type="text" name="name" placeholder="Name" />
-          <input type="email" name="email" placeholder="Email" />
-          <input type="text" name="street" placeholder="Street" />
-          <input type="text" name="postNo" placeholder="POST No" />
-          <Button btnType="success">ORDER</Button>
-        </form>
+        {form}
       </div>
     );
   }
